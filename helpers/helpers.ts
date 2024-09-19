92aabc99be919f134ba9026c0ae72dc4ad942eca
garold664 <garold664@gmail.com>
2024-09-19 21:48:17 +0300

    fix maxAge for cookies

--- helpers/helpers.ts
+++ helpers/helpers.ts
@@ -2,8 +2,8 @@ import { ResponseCookies } from 'next/dist/compiled/@edge-runtime/cookies';
 
 export function setAccessToken(store: ResponseCookies, token: string) {
   store.set('Access-Token', token, {
-    maxAge: 30,
-    // maxAge: 36000, // 10 hours
+    // maxAge: 30,
+    maxAge: 36000, // 10 hours
     httpOnly: true,
     path: '/',
     secure: true,
@@ -12,8 +12,8 @@ export function setAccessToken(store: ResponseCookies, token: string) {
 
 export function setRefreshToken(store: ResponseCookies, token: string) {
   store.set('Refresh-Token', token, {
-    maxAge: 36000,
-    // maxAge: 1728000, // 20 days
+    // maxAge: 36000,
+    maxAge: 1728000, // 20 days
     httpOnly: true,
     path: '/',
     secure: true,
